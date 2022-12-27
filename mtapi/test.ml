131ad122ce9dacbe754999b33f3d8a0f626916d1
George Shammas <george@shamm.as>
2022-12-27 18:22:53 -0500

    Cleanup comments

--- mtapi/test.ml
+++ mtapi/test.ml
@@ -4,11 +4,9 @@ open Mtaproto
 let ( >>= ) = Lwt.( >>= )
 let ( >|= ) = Lwt.( >|= )
 
-(* let f1 f p = f p
-   let f2 f a p = f p a *)
+let f1 f p = f p
+let f2 f a p = f p a
 let f3 f a b p = f p a b
-
-(* let (|>>>) p f a b = f p a b *)
 
 let headers =
   let api_key =
@@ -24,35 +22,12 @@ let body =
   Cohttp_lwt_unix.Client.get ?headers
     (Uri.of_string
        "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs")
-  >>= fun (_, body) ->
-  (* let code = resp |> Cohttp.Response.status |> Cohttp.Code.code_of_status in
-      Stdio.printf "Response code: %d\n" code;
-     Stdio.printf "Headers: %s\n"
-       (resp |> Cohttp.Response.headers |> Cohttp.Header.to_string); *)
-  body |> Cohttp_lwt.Body.to_string
-(* >|= fun body ->
-   (* Stdio.printf "Body of length: %d\n" (String.length body); *)
-   body *)
-
-(* let () =
-     let body = Lwt_main.run body in
-     Stdio.print_endline ("Received body\n" ^ body)
-
-   let foo : Models_t.complex =
-     {
-       n = [ { route = "test"; time = "test" } ];
-       s = [ { route = "test"; time = "test" } ];
-       id = "foo";
-       last_update = "foo";
-       name = "foo";
-       routes = [ "a"; "b" ];
-       stops = [ ("foo", [ 1.0 ]) ];
-     }
-
-   let () = Stdio.print_endline (Models_j.string_of_complex foo) *)
+  >>= fun (_, body) -> body
+  |> Cohttp_lwt.Body.to_string
 
 let () =
   let body = Lwt_main.run body in
   Pbrt.Decoder.of_string body
   |> Gtfs_realtime_pb.decode_feed_message
-  |> Gtfs_realtime_json.string_of_feed_message |> Stdio.print_endline
+  |> Gtfs_realtime_json.string_of_feed_message
+  |> Stdio.print_endline
